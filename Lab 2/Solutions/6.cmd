350b2c873cdbc2fffe9172def7f7e0651580f966
Konrad Gumny <246638@student.pwr.edu.pl>
2020-10-15 22:06:04 +0200

    Update 6.cmd

--- Lab 2/Solutions/6.cmd
+++ Lab 2/Solutions/6.cmd
@@ -6,21 +6,21 @@ Exit /b 0
 
 :Print_All_Files
     
-    call :Print_Files_With_Level %~1 1
+    call :Print_Files_With_Level "%~1" 1
     
-    for /f %%B in ('dir "%~1" /ad /b') do (
+    for /f "usebackq delims=?" %%B in (`dir "%~1" /ad /b`) do (
         echo %%B
-        call :Print_Dir_Recursive %~1\%%B 1
+        call :Print_Dir_Recursive "%~1\%%B" 1
     )
     Exit /B 0
 :Print_Files_With_Level
-
+    
     set "str=─"
     for /L %%X in (1,1,%~2) do set "str=!str!─"
     set "white= "
     for /L %%X in (1,1,%nextLevel%) do set "white=!white!  "
-    
-    for /f "usebackq delims=?" %%A in (`dir "%~1" /a-d-h-o-i /b`) do (
+
+    for /f "usebackq delims=?" %%A in (`dir "%~1" /a-d /b`) do (
         if %~2 == 1 (echo ─%%A) else (echo %white%└!str!%%A)
     )
 
@@ -35,9 +35,7 @@ Exit /b 0
     for /L %%X in (1,1,%nextLevel%) do set "white=!white!  "
         
     for /f "usebackq delims=?" %%B in (`dir "%~1" /ad /b`) do (
-        
         echo !white!%%B
-        
         call :Print_Dir_Recursive "%~1\%%B" %nextLevel%
         
     )
